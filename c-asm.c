1fce6981f057d5f663af7f46f744bc9315f7d7cc
GeorgeDaglaridis <george_daglaridis@yahoo.com>
2026-02-10 12:26:49 +0000

    exploring datatypes int/float singne/unsigned

--- c-asm.c
+++ c-asm.c
@@ -1,3 +1,25 @@
 int piyush(){
+
+    //char c = 'A';
+    char n1 = 255; // the limit of 1-byte unsigned number
+    //char n2 = -128; // the limit of 1-byte signed number
+    //char n3 = -129; // beyond the limits of 1-byte signed number
+    //int variable1 = 1234;
+    //int variable2 = 5678;
+
+    //float float_var1 = 2.0;
+    //float float_var2 = -2.0;
+    //float float_var3 = 0.5;
+    //float float_var4 = -0.5;
+    //float float_var5 = 1.25;
+    //float float_var6 = 3.0;
+    //float float_var7 = 5.0;
+    //float float_var8 = 5.6;
+
+    // when whether a variable is signed or not is not defined
+    // compiler considers that variable as a signed value
+    // unsigned short random_value = 123456576;
+
+
     return 3;
 }
