fc748e58e6954e5c0d6ad0be0c60c8467c529a4a
GeorgeDaglaridis <george_daglaridis@yahoo.com>
2026-02-14 18:45:44 +0000

    exploring static C keyword

--- expt/other.c
+++ expt/other.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int a = 10;
+static int a = 10;
 
-void foo() {
+static void foo() {
     a = a + 1;
     printf("foo: %d\n", a);
 
     return;
+}
+
+void bar() {
+    static char b = 3;
+    b = b + 1;
+
+    foo();
+
+    printf("bar: %d\n", b);
+    return;
+    
 }
---
+++ expt/static.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern void bar();
+
+void main() {
+    bar();
+    bar();
+  
+    return;
+}
