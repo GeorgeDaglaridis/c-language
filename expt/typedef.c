8189601ac247bf0a4e5b7e07bf1fdd8f090a3826
GeorgeDaglaridis <george_daglaridis@yahoo.com>
2026-02-14 12:55:07 +0000

    j++ vs ++j

--- expt/typedef.c
+++ expt/typedef.c
@@ -25,7 +25,7 @@ void main(){
     printf("%ld\n", sizeof(union pixelu_t));
     */
 
-    //int i = 10;
+    int i = 10;
     int j = 2;
     // while ( i > 0 ){
     //     if (i == 5){
@@ -47,12 +47,23 @@ void main(){
     }
     */
 
+
     // I thought it should have been different but in both cases, i-- or --i,
     // i gets decremented after the body of the loop is executed
+    /*
     for (int i = 10; i > 0; --i){
         printf("i gets decremented before the body of the loop is executed, i = %d\n", i);
         j = j + 200;
     }
+    */
+
+    int i = 10;
+    int j = 2;
+    printf("i before, i = %d\n", i);
+    printf("j before, i = %d\n", j);
+    i = ++j + 1;
+    printf("i after, i = %d\n", i);
+    printf("j after, j = %d\n", j);
 
     return;
 }
