149d738f08a16fe577d1122b90d331b94e1eea3e
GeorgeDaglaridis <george_daglaridis@yahoo.com>
2026-02-14 14:19:38 +0000

    exploring global variables

---
+++ expt/global.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+
+int a = 10;
+
+/*
+void foo() {
+    a = a + 1;
+    printf("foo: %d\n", a);
+
+    return;
+}
+*/
+
+void main() {
+    a = a + 1;
+    //printf("main: %d\n", a);
+  
+    //foo();
+    return;
+}
---
+++ expt/global.s
@@ -0,0 +1,33 @@
+	.file	"global.c"
+	.option nopic
+	.attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.globl	a
+	.section	.sdata,"aw"
+	.align	2
+	.type	a, @object
+	.size	a, 4
+a:
+	.word	10
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-16
+	sd	s0,8(sp)
+	addi	s0,sp,16
+	lui	a5,%hi(a)
+	lw	a5,%lo(a)(a5)
+	addiw	a5,a5,1
+	sext.w	a4,a5
+	lui	a5,%hi(a)
+	sw	a4,%lo(a)(a5)
+	nop
+	ld	s0,8(sp)
+	addi	sp,sp,16
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (13.2.0-11ubuntu1+12) 13.2.0"
