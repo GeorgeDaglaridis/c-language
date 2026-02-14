edcea70dbe695069b337d98820783216ca8e03d5
GeorgeDaglaridis <george_daglaridis@yahoo.com>
2026-02-14 11:32:18 +0000

    exploring brancing and looping

--- c-asm.c
+++ c-asm.c
@@ -1,22 +1,54 @@
 //#include <stdio.h>
 
-int piyush(){
+void piyush(){
 
-    struct pixel_t {
-        unsigned int l;
-        unsigned char r;
-        unsigned char g;
-        unsigned char b;
+    int i = 11;
+    //int j = 2;
+
+    /*
+    if ( i==11 ){
+        goto end;
+    }
+
+    i = i + 2;
+
+    end:
+        i = i + 21;
+    */
+
+    /*
+    if (i == 1) {
+        j = j + 1;
+    } else if ( i== 2) {
+        j = j + 2;
+    } else {
+        j= j + 3;
+    } 
+    */
     
-    };
-
-    struct pixel_t pixel;
-    pixel.l = 4;
-    pixel.r = 255;
-    pixel.g = 253;
-    pixel.b = 252;
+    /*
+    switch (i)
+    {
+        case 1:
+            j = j + 1;
+            break;
+        case 2:
+            j = j + 2;
+            break;
+        case 3:
+            j = j + 3;
+            break;
+        default:
+            j = j + 100;
+            break;
+    }
+    */
     
-    //printf("%ld\n", sizeof(pixel));
+    /*
+    for (int i = 10; i > 0; --i){
+        j = j + 200;
+    }
+    */        
 
-    return 0;
+    return;
 }
--- expt/typedef.c
+++ expt/typedef.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*
 struct pixel_t {
     unsigned int l;
     unsigned char r;
@@ -12,13 +13,46 @@ union pixelu_t {
     unsigned char r;
     unsigned short g;    
 };
+*/
 
 void main(){
 
+    /*
     struct pixel_t pixel;
     union pixelu_t pixelu;
     
     //printf("%ld\n", sizeof(struct pixel_t));
     printf("%ld\n", sizeof(union pixelu_t));
+    */
 
+    //int i = 10;
+    int j = 2;
+    // while ( i > 0 ){
+    //     if (i == 5){
+    //         i = i-1;
+    //         continue;
+    //     }
+    //     printf("When continue is used, i = %d\n", i);
+    //     i = i-1;  
+    // }
+    
+    /*
+    while ( i > 0 ){
+        if (i == 5){
+            i = i-1;
+            break;
+        }
+        printf("When break is used, i = %d\n", i);
+        i = i-1;  
+    }
+    */
+
+    // I thought it should have been different but in both cases, i-- or --i,
+    // i gets decremented after the body of the loop is executed
+    for (int i = 10; i > 0; --i){
+        printf("i gets decremented before the body of the loop is executed, i = %d\n", i);
+        j = j + 200;
+    }
+
+    return;
 }
