b0c65401956bf5135b8e1e4b6ad706d7064e8c24
George Thedoros P. Daglaridis <george_daglaridis@yahoo.com>
2026-03-09 12:38:36 +0200

    exploring operators

---
+++ expt/operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    char a = 127;
+    char b = 4;
+    //printf("++a : %d\n", ++a);
+    //printf("++a : %d\n", ++a);
+    char c = a + b;
+    printf("a + b : %d\n", a + b);
+    printf("c = a + b : %d\n", c);
+}
